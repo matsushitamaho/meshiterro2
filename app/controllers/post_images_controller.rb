2b874b46b77a8805f8cbd4a7d4c289f47b6c5fe9
mathushitamaho <mapo.maho.nissy0610@gmail.com>
2023-08-21 06:53:56 +0000

    コメント/いいね/投稿削除のエラー未解決

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -23,8 +23,8 @@ class PostImagesController < ApplicationController
   end
   
   def destroy
-    post_image = PostImage.find(params[:id])
-    post_image.destroy
+    #@post_image = PostImage.find(params[:id])
+    @post_image.destroy
     redirect_to post_images_path
   end
   
